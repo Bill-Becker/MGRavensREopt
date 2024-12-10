75689e0363f13205e27c0daf964245b01722842c
Bill Becker <William.Becker@nrel.gov>
2024-12-09 22:28:39 -0700

    Move inputs-manipulation code over into that designated file

--- mgravens_run.jl
+++ mgravens_run.jl
@@ -29,54 +29,12 @@ DotEnv.load!()
 # Only need the line below if calling this .jl file from within Julia, as opposed to command line executing which takes the CLI ARGS
 ARGS_local = deepcopy(Base.ARGS)
 if isempty(ARGS_local)
-    ARGS_local = ["REopt_Lehigh_v4.json", "REopt_Lehigh_v4.json"]
+    ARGS_local = ["REopt_Lehigh_v4_12.09.json", "REopt_Lehigh_v4_created.json"]
 end
 
 # Load in the MG-Ravens .json schema file, which is the only user input and customized for the scenario to run
 input_file_path = ARGS_local[1]
 mgravens = JSON.parsefile(input_file_path)
-
-# Temporarily manipulate the lehigh_example_v1.json data for energy prices, loads, and PV production profile because they are non-sensical
-# Copied the printout of named values from the lehigh_example_v1.json for overwriting values from .csv or other source from lehigh4mgs->reopt_mg0
-# site_name = "proposedSite1"
-# load_groups = Any["ResidentialGroup"]
-# energy_consumer_names = Any["670a_residential2"]
-# load_forecast_names = Any["670a_residential2_shape"]
-# subregion_name = "lehigh_SubRegion"
-# region_name = "lehigh_Region"
-# lmp_name = "lmps_lehigh"
-# capacity_prices_name = "capacityprices_lehigh"
-
-# Load more custom data for example, such as load profiles and other
-# loads_kw = readdlm("LoadShape.csv", ',', header=false)[:,]  # Need the [:,] at the end to convert matrix to vector
-# critical_loads_kw = readdlm("criticalLoadShape.csv", ',', header=false)[:,]   # NOT USED, USED 50% critical load fraction instead
-# inputs_data = JSON.parsefile("julia_default.json")
-
-# mgravens["BasicIntervalSchedule"][load_forecast_names[1]]["EnergyConsumerSchedule.RegularTimePoints"] = []
-# mgravens["EnergyPrices"]["LocationalMarginalPrices"][lmp_name]["LocationalMarginalPrices.LMPCurve"]["PriceCurve.CurveDatas"] = []
-# for ts in 1:8760
-#     # Update load
-#     updated_load = Dict(
-#         "RegularTimePoint.sequenceNumber"=> ts,
-#         "RegularTimePoint.value2"=> 0.0,
-#         "RegularTimePoint.value1"=> loads_kw[ts]
-#         )   
-#     append!(mgravens["BasicIntervalSchedule"][load_forecast_names[1]]["EnergyConsumerSchedule.RegularTimePoints"], [updated_load])
-#     # Update LMP
-#     updated_lmp = Dict(
-#         "CurveData.xvalue"=> ts-1, 
-#         "CurveData.y1value"=> inputs_data["ElectricTariff"]["blended_annual_energy_rate"]
-#     )
-#     append!(mgravens["EnergyPrices"]["LocationalMarginalPrices"][lmp_name]["LocationalMarginalPrices.LMPCurve"]["PriceCurve.CurveDatas"], [updated_lmp])
-# end
-# # Update capacity prices
-# for month in 1:12
-#     mgravens["EnergyPrices"]["CapacityPrices"][capacity_prices_name]["CapacityPrices.CapacityPriceCurve"]["PriceCurve.CurveDatas"][month]["CurveData.y1value"] = inputs_data["ElectricTariff"]["blended_annual_demand_rate"]
-# end
-
-# open("modified_lehigh.json","w") do f
-#     JSON.print(f, mgravens)
-# end
 
 # Convert MG-Ravens data schema into REopt schema
 reopt_inputs = convert_mgravens_inputs_to_reopt_inputs(mgravens)
