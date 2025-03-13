0e2e7c8b316c828503b1980c06d77b9a48c4334e
wbecker <william.becker@nrel.gov>
2025-03-13 13:02:24 -0600

    Update script for building required inputs for REopt into the mgravens JSON

--- manipulate_mgravens.jl
+++ manipulate_mgravens.jl
@@ -4,99 +4,176 @@ using DelimitedFiles
 using DataFrames
 using CSV
 
-mgravens = JSON.parsefile("nda-hce-feeder-interval.json")
+"""
+This script is an example of how to manipulate the mgravens JSON file to add custom data such as load profiles, energy prices, etc.
+This should check for the existing of all needed keys and values in the mgravens JSON file for REopt, and add them if they don't exist.
+"""
+
+# Load the existing mgravens JSON file to add/manipulate
+mgravens = JSON.parsefile("nda-hce-feeder-03062025.json")
+
+# Template files for adding keys/sections to the mgravens JSON file
+algoprops_template = JSON.parsefile("template_algoprops.json")
+proposedassetoption_template = JSON.parsefile("template_proposedassetoption.json")
+proposedsitelocation_template = JSON.parsefile("template_proposedsitelocations.json")
+region_template = JSON.parsefile("template_region.json")
+loadgroup_template = JSON.parsefile("template_loadgroup.json")
 lmp_template = JSON.parsefile("template_lmp.json")
 cap_template = JSON.parsefile("template_capacity.json")
 cp_template = JSON.parsefile("template_cp.json")
+outages_template = JSON.parsefile("template_outages.json")
 
-energy_prices_template = Dict(
-    "LocationalMarginalPrices" => Dict(),
-    "CapacityPrices" => Dict(),
-    "CoincidentPeakPrices" => Dict()
-    )
-mgravens["EnergyPrices"] = energy_prices_template
-# Note, LocationalMarginalPrices and CoincidentPeakPrices have just ONE entry in their timeseries/array object, to be overwritten and appended
-#   while CapacityPrices has 12 entries already, one for each month, which should typically just be updated with new values
-mgravens["EnergyPrices"]["LocationalMarginalPrices"] = lmp_template["LocationalMarginalPrices"]
-mgravens["EnergyPrices"]["CapacityPrices"] = cap_template["CapacityPrices"]
-mgravens["EnergyPrices"]["CoincidentPeakPrices"] = cp_template["CoincidentPeakPrices"]
-# Replace string segments "_name" in the key names or values of the mgravens["EnergyPrices"] dictionary with a variable to be input
-function replace_name_segments!(dict, variable)
-    for (key, value) in dict
-        if isa(value, Dict)
-            replace_name_segments!(value, variable)
-        elseif isa(value, String)
-            dict[key] = replace(value, "_name" => variable)
-        end
-        if isa(key, String) && occursin("_name", key)
-            new_key = replace(key, "_name" => variable)
-            dict[new_key] = dict[key]
-            delete!(dict, key)
-        end
-    end
-end
-
-replace_name_segments!(mgravens["EnergyPrices"], "_hce")
-
-# # Load more custom data for example, such as load profiles and other
-# load_data = CSV.read("airport_feederhead_scada.csv", DataFrame)
-# loads_kw = load_data[!, "Airport_Ckt_MW_feederhead"]
-# critical_loads_kw = load_data[!, "Airport_Ckt_MW_entire_microgrid"]
-
-site_name = "hce_feeder"
-# load_groups = Any["ResidentialGroup"]
-# energy_consumer_names = Any["hce_residential"]
-# load_forecast_names = Any["hce_residential_shape"]
+# Proper noun/names to be assigned to this particular use case
+site_name = "proposedSite1"
+load_groups = []  # e.g. ["ResidentialGroup", "IndustrialGroup"]; if empty, don't rely on LoadGroup to find EnergyConsumers and instead aggregate all EnergyConsumers
+energy_consumer_names = collect(keys(mgravens["PowerSystemResource"]["Equipment"]["ConductingEquipment"]["EnergyConnection"]["EnergyConsumer"]))
+# load_forecast_names = ["hce_residential_shape"]
 subregion_name = "hce_SubRegion"
 region_name = "hce_Region"
 lmp_name = "lmps_hce"
 capacity_prices_name = "capacityprices_hce"
 cp_prices_name = "coincidentpeakprices_hce"
 
-lmp_price = 0.1
-capacity_prices_monthly = ones(12) * 0.0
-cp_price_monthly = 11.0 # $11/kW-month, but must specify 12 "active" timesteps for it to charge monthly, otherwise it will just charge once yearly
-
-# TODO write the load and lmps arrays to the nda-hce-feeder-reopt.json file
-
-# mgravens["BasicIntervalSchedule"][load_forecast_names[1]]["EnergyConsumerSchedule.RegularTimePoints"] = []
-mgravens["EnergyPrices"]["LocationalMarginalPrices"][lmp_name]["LocationalMarginalPrices.LMPCurve"]["PriceCurve.CurveDatas"] = []
-for ts in 1:8760
-    # Update load
-    # updated_load = Dict(
-    #     "RegularTimePoint.sequenceNumber"=> ts,
-    #     "RegularTimePoint.value2"=> 0.0,
-    #     "RegularTimePoint.value1"=> loads_kw[ts]
-    #     )   
-    # append!(mgravens["BasicIntervalSchedule"][load_forecast_names[1]]["EnergyConsumerSchedule.RegularTimePoints"], [updated_load])
-    # Update LMP
-    updated_lmp = Dict(
-        "CurveData.xvalue"=> ts-1, 
-        "CurveData.y1value"=> lmp_price
-    )
-    append!(mgravens["EnergyPrices"]["LocationalMarginalPrices"][lmp_name]["LocationalMarginalPrices.LMPCurve"]["PriceCurve.CurveDatas"], [updated_lmp])
-end
-
-#  Update capacity and coincident peak prices
-global hour_count = 0
-active_day_for_cp = 15
-active_hour_for_cp = 14
-for month in 1:12
-    mgravens["EnergyPrices"]["CapacityPrices"][capacity_prices_name]["CapacityPrices.CapacityPriceCurve"]["PriceCurve.CurveDatas"][month]["CurveData.y1value"] = capacity_prices_monthly[month]
-    # While CapacityPrices always get updated each month, in this case we're also updating CoincidentPeakPrices each month based on HCE billing 
-    cp_hour = hour_count + active_day_for_cp * 24 + active_hour_for_cp
-    updated_cp = Dict(
-        "CurveData.xvalue"=> cp_hour, 
-        "CurveData.y1value"=> cp_price_monthly
-    )
-    if month == 1
-        mgravens["EnergyPrices"]["CoincidentPeakPrices"][cp_prices_name]["CoincidentPeakPrices.CoincidentPeakPriceCurve"]["PriceCurve.CurveDatas"][month] = updated_cp
-    else
-        append!(mgravens["EnergyPrices"]["CoincidentPeakPrices"][cp_prices_name]["CoincidentPeakPrices.CoincidentPeakPriceCurve"]["PriceCurve.CurveDatas"], [updated_cp])
+# Replace string segments placeholder in the key names or values of the mgravens["EnergyPrices"] dictionary with a variable to be input
+function replace_name_segments!(dict, placeholder, name)
+    for (key, value) in dict
+        if isa(value, Dict)
+            replace_name_segments!(value, placeholder, name)
+        elseif isa(value, String)
+            dict[key] = replace(value, placeholder => name)
+        end
+        if isa(key, String) && occursin(placeholder, key)
+            new_key = replace(key, placeholder => name)
+            dict[new_key] = dict[key]
+            delete!(dict, key)
+        end
     end
-    global hour_count += round(Int, 30.5 * 24)
+    return dict
 end
 
-open("energy_prices_hce.json","w") do f
-    JSON.print(f, mgravens["EnergyPrices"])
+if isnothing(get(mgravens, "AlgorithmProperties", nothing))
+    mgravens["AlgorithmProperties"] = algoprops_template["AlgorithmProperties"]
 end
+
+
+if isnothing(get(mgravens, "ProposedAssetOption", nothing))
+    mgravens["ProposedAssetOption"] = proposedassetoption_template["ProposedAssetOption"]
+end
+
+# Remove PV GenerationProfile from template to use PVWatts within REopt
+delete!(mgravens["ProposedAssetOption"]["ProposedEnergyProducerOption"]["proposedPV1"], "ProposedPhotovoltaicUnitOption.GenerationProfile")
+
+# Manipulate other PV or Battery inputs here
+
+if isnothing(get(mgravens, "ProposedSiteLocations", nothing))
+    mgravens["ProposedSiteLocations"] = Dict(site_name => replace_name_segments!(proposedsitelocation_template["ProposedSiteLocations"][site_name], "name_SubRegion", subregion_name))
+end
+
+# Make ProposedSiteLocation.LoadGroup empty for this use case because this assumes all EnergyConsumers get aggregated
+mgravens["ProposedSiteLocations"][site_name]["ProposedSiteLocation.LoadGroup"] = load_groups
+
+# Outages - would REopt choose four outages centered around peak load?
+if isnothing(get(mgravens, "OutageScenario", nothing))
+    mgravens["OutageScenario"] = outages_template["OutageScenario"]
+end
+
+# TODO there may be a Group.LoadGroup in the starting .json, but it may have one entry with key="NONE" which is equivalent to omitting
+if !isempty(load_groups)
+    if isnothing(get(mgravens["Group"], "LoadGroup", nothing))
+        mgravens["Group"]["LoadGroup"] = loadgroup_template["LoadGroup"]
+    end
+    loadgroup_template_generic = pop!(mgravens["Group"]["LoadGroup"], "NameGroup")
+    for (i, load_group) in enumerate(load_groups)
+        mgravens["Group"]["LoadGroup"][load_group] = replace_name_segments!(loadgroup_template_generic, "NameGroup", load_group)
+        mgravens["Group"]["LoadGroup"][load_group]["LoadGroup.EnergyConsumers"] = ["EnergyConsumer::'"*energy_consumer_names[ec]*"'" for ec in eachindex(energy_consumer_names)]
+    end
+end
+
+# The region_template.json contains both Group.SubGeographicalRegion and Group.GeographicalRegion
+if isnothing(get(mgravens["Group"], "SubGeographicalRegion", nothing))
+    mgravens["Group"]["SubGeographicalRegion"] = Dict(subregion_name => region_template["SubGeographicalRegion"][subregion_name])
+end
+if isnothing(get(mgravens["Group"], "GeographicalRegion", nothing))
+    mgravens["Group"]["GeographicalRegion"] = Dict(region_name => region_template["GeographicalRegion"][region_name])
+end
+
+
+###   ENERGY PRICES   ###
+energy_prices_template = Dict(
+    "LocationalMarginalPrices" => Dict(lmp_name => Dict()),
+    "CapacityPrices" => Dict(capacity_prices_name => Dict()),
+    "CoincidentPeakPrices" => Dict(cp_prices_name => Dict())
+    )
+
+created_energy_prices = false
+if isnothing(get(mgravens, "EnergyPrices", nothing))
+    created_energy_prices = true
+    # Note, LocationalMarginalPrices and CoincidentPeakPrices have just ONE entry in their template timeseries/array object, to be overwritten and appended
+    #   while CapacityPrices has 12 entries already, one for each month, which should typically just be updated with new values
+    mgravens["EnergyPrices"] = energy_prices_template
+    mgravens["EnergyPrices"]["LocationalMarginalPrices"][lmp_name] = lmp_template["LocationalMarginalPrices"]["lmps_name"]
+    mgravens["EnergyPrices"]["CapacityPrices"][capacity_prices_name] = cap_template["CapacityPrices"]["capacityprices_name"]
+    mgravens["EnergyPrices"]["CoincidentPeakPrices"][cp_prices_name] = cp_template["CoincidentPeakPrices"]["coincidentpeakprices_name"]
+    mgravens["EnergyPrices"] = replace_name_segments!(mgravens["EnergyPrices"], "_name", "_hce")
+else
+    @warn "EnergyPrices exists in mgravens.json file, so not adding any of LMP, Capacity, or CP prices"
+end
+
+if created_energy_prices
+    lmp_price = 0.06 # $0.06/kWh
+    capacity_prices_monthly = ones(12) * 0.0
+    cp_price_monthly = 11.0 # $11/kW-month, but must specify 12 "active" timesteps for it to charge monthly, otherwise it will just charge once yearly
+    active_day_for_cp = 15
+    active_hour_for_cp = 17
+
+    # mgravens["BasicIntervalSchedule"][load_forecast_names[1]]["EnergyConsumerSchedule.RegularTimePoints"] = []
+    mgravens["EnergyPrices"]["LocationalMarginalPrices"][lmp_name]["LocationalMarginalPrices.LMPCurve"]["PriceCurve.CurveDatas"] = []
+    for ts in 1:8760
+        # Update load
+        # updated_load = Dict(
+        #     "RegularTimePoint.sequenceNumber"=> ts,
+        #     "RegularTimePoint.value2"=> 0.0,
+        #     "RegularTimePoint.value1"=> loads_kw[ts]
+        #     )   
+        # append!(mgravens["BasicIntervalSchedule"][load_forecast_names[1]]["EnergyConsumerSchedule.RegularTimePoints"], [updated_load])
+        # Update LMP
+        updated_lmp = Dict(
+            "CurveData.xvalue"=> ts-1, 
+            "CurveData.y1value"=> lmp_price
+        )
+        append!(mgravens["EnergyPrices"]["LocationalMarginalPrices"][lmp_name]["LocationalMarginalPrices.LMPCurve"]["PriceCurve.CurveDatas"], [updated_lmp])
+    end
+
+    #  Update capacity and coincident peak prices
+    global hour_count = 0
+    for month in 1:12
+        mgravens["EnergyPrices"]["CapacityPrices"][capacity_prices_name]["CapacityPrices.CapacityPriceCurve"]["PriceCurve.CurveDatas"][month]["CurveData.y1value"] = capacity_prices_monthly[month]
+        # While CapacityPrices always get updated each month, in this case we're also updating CoincidentPeakPrices each month based on HCE billing 
+        cp_hour = hour_count + active_day_for_cp * 24 + active_hour_for_cp
+        updated_cp = Dict(
+            "CurveData.xvalue"=> cp_hour, 
+            "CurveData.y1value"=> cp_price_monthly
+        )
+        if month == 1
+            mgravens["EnergyPrices"]["CoincidentPeakPrices"][cp_prices_name]["CoincidentPeakPrices.CoincidentPeakPriceCurve"]["PriceCurve.CurveDatas"][month] = updated_cp
+        else
+            append!(mgravens["EnergyPrices"]["CoincidentPeakPrices"][cp_prices_name]["CoincidentPeakPrices.CoincidentPeakPriceCurve"]["PriceCurve.CurveDatas"], [updated_cp])
+        end
+        global hour_count += round(Int, 30.5 * 24)
+    end
+end
+
+open("nda-hce-reopt-inputs.json","w") do f
+    JSON.print(f, mgravens)
+end
+
+
+# mgravens = JSON.parsefile("lehigh_v8_corrected.json")
+# lmp_name = "lmps_lehigh"
+# lmp_dict = get(mgravens["EnergyPrices"]["LocationalMarginalPrices"], lmp_name, nothing)
+# lmp_list_of_dict = lmp_dict["LocationalMarginalPrices.LMPCurve"]["PriceCurve.CurveDatas"]
+# wanted_list = lmp_list_of_dict[1:24]
+# mgravens["EnergyPrices"]["LocationalMarginalPrices"][lmp_name]["LocationalMarginalPrices.LMPCurve"]["PriceCurve.CurveDatas"] = wanted_list
+# open("lehigh_v8_shorten_lmp.json","w") do f
+#     JSON.print(f, mgravens)
+# end
